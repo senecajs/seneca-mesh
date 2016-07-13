882b977f845f9e381e88e9dd96bd47300ac25e5c
Richard Rodger <richard@ricebridge.com>
2016-07-13 18:06:30 +0100

    docker part 4

--- test/base.js
+++ test/base.js
@@ -3,11 +3,13 @@
 
 var HOST = process.env.HOST || process.argv[2]
 var BASES = (process.env.BASES || process.argv[3] || '').split(',')
+var PORT = process.env.PORT
 
 require('seneca')({tag:'b0'})
   .use('..',{
     isbase: true, 
     host: HOST,
+    port: PORT,
     bases: BASES,
     sneeze: {
       silent: false

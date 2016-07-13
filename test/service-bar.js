c6229c5314011ff3c2b3e27f5152dd71e8fb4a5e
Richard Rodger <richard@ricebridge.com>
2016-07-13 16:22:51 +0100

    docker part 1

--- test/service-bar.js
+++ test/service-bar.js
@@ -1,8 +1,23 @@
+// To run:
+// $ node service-foo.js
+
+var HOST = process.env.HOST || process.argv[2]
+var BASES = (process.env.BASES || process.argv[3] || '').split(',')
+
 require('seneca')({tag:'bar'})
   .add( 'bar:1', function (msg, done) {
     done( null, {y:1,v:100+msg.v} )
   })
-  .use('..', { pin:'bar:1', sneeze:{silent:false} })
+  .use('..', {
+    host: HOST,
+    bases: BASES,
+    listen: [{
+      pin: 'bar:1'
+    }],
+    sneeze: {
+      silent: false
+    } 
+  })
 
   .ready( function () {
     var seneca = this
